0f4db4aaeaf45ae58cd7e663916313f69310f586
Mark Voorberg <mark@binaryops.ca>
2019-03-14 14:15:51 -0700

    updates to be way more flexible.

--- index.js
+++ index.js
@@ -1,45 +1,59 @@
 'use strict';
-
-const hndl = require('./lib/captchaHandler');
+const handler = require('./lib/captchaHandler');
 
 function captchaInit(options) {
-    const CAPTCHA_TOKEN_HEADER =
-        options.CAPTCHA_TOKEN_HEADER || 'Authorization'; // the request header where we expect the jwt token
-    const CAPTCHA_NONCE_HEADER =
-        options.CAPTCHA_NONCE_HEADER || 'captcha-nonce'; // the request header where we expect the client nonce
+  options = options || {};
+  // the request header where we expect the jwt token
+  const tokenHeader = options.tokenHeader || 'Authorization';
+  // the request header where we expect the client nonce
+  const nonceHeader = options.nonceHeader || 'captcha-nonce';
 
-    const captchaHandler = hndl(options);
+  const captchaHandler = handler({
+    tokenSecret: options.tokenSecret,
+    expiryMinutes: options.expiryMinutes,
+    languageMap: options.languageMap,
+    // SVG Captcha configs
+    captchaSize: options.captchaSize,
+    ignoreChars: options.ignoreChars,
+    noise: options.noise,
+    width: options.width,
+    height: options.height,
+    background: options.background,
+    color: options.color,
+    inverse: options.inverse,
+    fontSize: options.fontSize
+  });
 
-    return {
-        getCaptcha: function(req, res, next) {
-            var fullReponse = captchaHandler.createCaptcha(req.body.nonce);
-            res.send(fullReponse);
-            next();
-        },
-        verifyCaptcha: function(req, res, next) {
-            var ret = captchaHandler.verifyCaptcha(req.body);
-            res.send(ret);
-            next();
-        },
-        getCaptchaAudio: function(req, res, next) {
-            captchaHandler.getAudio(req.body, req).then(function(ret) {
-                res.send(ret);
-                next();
-            });
-        },
-        verifyJWTResponseMiddleware: function(req, res, next) {
-            var token = req.headers[CAPTCHA_TOKEN_HEADER.toLowerCase()] || '';
-            token = token.replace('Bearer ', '');
-            var nonce = req.headers[CAPTCHA_NONCE_HEADER];
-            var ret = captchaHandler.verifyJWTResponse(token, nonce);
-            if (ret.valid) {
-                next();
-            } else {
-                res.send(401, 'Not Authorized');
-                next('Invalid Captcha Token');
-            }
-        }
-    };
+  return {
+    getCaptcha: function(req, res, next) {
+      const fullReponse = captchaHandler.createCaptcha(req.body.nonce);
+      res.send(fullReponse);
+      next();
+    },
+    verifyCaptcha: function(req, res, next) {
+      const ret = captchaHandler.verifyCaptcha(req.body);
+      res.send(ret);
+      next();
+    },
+    getCaptchaAudio: function(req, res, next) {
+      captchaHandler.getAudio(req.body, req).then(ret => {
+        res.send(ret);
+        next();
+      });
+    },
+    verifyJWTResponseMiddleware: function(req, res, next) {
+      let token = req.headers[tokenHeader.toLowerCase()] || '';
+      token = token.replace('Bearer ', '');
+      const nonce = req.headers[nonceHeader];
+      const ret = captchaHandler.verifyJWTResponse(token, nonce);
+      if (ret.valid) {
+        next();
+      } else {
+        res.send(401, 'Not Authorized');
+        next('Invalid Captcha Token');
+      }
+    }
+  };
 }
 
 module.exports = captchaInit;
